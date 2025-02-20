24497179098823ebd28adfca708846b86177027c
Janan <02.95.janan@gmail.com>
2025-02-21 03:48:13 +0500

    First Assgnment for JavaScript From SMIT

---
+++ script.js
@@ -0,0 +1,61 @@
+alert("WelCome to My Page!");
+alert("Error! Please enter a valid password.");
+alert("Welcome To JS Land...\nHappy Coding!");
+
+// Declare a varible
+var userName;
+
+// Declare a variable & assign STRING
+var myName = "Janan";
+console.log(myName);
+
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign "Hello World" to variable message
+// c) Display the message in alert box.
+var message;
+message = "Hello World";
+alert(message);
+
+// write a script to save student's bio data in variable 
+// and show the data in alert box.
+var studentName = "Janan";
+var age = 29;
+var course = "WebApp Development";
+var institute = "SMIT";
+
+var bioData = "Student Name: " + studentName + "\n"
+            + "Age: " + age + "\n"
+            + "Course: " + course + "\n"
+            + "Institute: " + institute;
+alert(bioData);
+
+// Q = ?
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// declare variable and give string e.g email and concatenate and alert.
+var email = "02.95.janan@gmail.com";
+alert("My Email Is -> " + email);
+
+// decleare a varible and give some text and display with a message.
+var express = "A Smarter Way to Learn JavaScript."
+alert("I am Trying to Learn from the Book " + express);
+
+// write a script to display some text in the browser through JS.
+document.write("I can write HTML through JavaScript. </br>");
+
+// take two number & sum.
+var a = 3;
+var b = 5;
+document.write(a + b);
+document.write("</br>", a - b);
+document.write("</br>", a * b);
+document.write("</br>", a / b);
+
+// ?
+var movieTicket = 600;
+document.write("</br>", movieTicket * 5);
+
+
+
